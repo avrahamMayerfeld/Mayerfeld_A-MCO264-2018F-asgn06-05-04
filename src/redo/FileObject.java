510c344fb97aa85ffb54bf61612684581b8b97dd
avrahamMayerfeld <44050391+avrahamMayerfeld@users.noreply.github.com>
2019-05-21 23:21:59 -0400

    fixed github indent corruption

--- src/redo/FileObject.java
+++ src/redo/FileObject.java
@@ -1,46 +1,44 @@
 package redo;
 
 
-	import java.util.Date;
+import java.util.Date;
 import java.util.PriorityQueue;
 import java.util.Random;
 
-	public class FileObject extends DiskObject{
+public class FileObject extends DiskObject{
         
-		String data;
-		Random rand =new Random();
-		Date currDate = new Date();
+	String data;
+	Random rand =new Random();
+	Date currDate = new Date();
 		
-		public FileObject(String name,String data) {
-			this.data = data;
-		    this.creator = System.getProperty("user.name");
-			this.created = currDate;
-			this.lastModified = currDate;
-			this.lastAccessed = currDate;
-			this.diskLocation = rand.nextInt();
-			this.name = name;
-		}
+	public FileObject(String name,String data) {
+		this.data = data;
+		this.creator = System.getProperty("user.name");
+		this.created = currDate;
+		this.lastModified = currDate;
+		this.lastAccessed = currDate;
+		this.diskLocation = rand.nextInt();
+		this.name = name;
+	}
 		
-		public void setData(String d){
-			data = d;
-			this.lastModified = currDate;
-			this.lastAccessed = currDate;
-		}
+	public void setData(String d){
+		data = d;
+		this.lastModified = currDate;
+		this.lastAccessed = currDate;
+	}
 		
-		public String getData() {
-			this.lastAccessed = currDate;
-			return data;
-		}
+	public String getData() {
+		this.lastAccessed = currDate;
+		return data;
+	}
 		
-		public String toString() {
-			return "File name: "+ name + " Data: "+ data+ "  "+ size();
-		}
+	public String toString() {
+		return "File name: "+ name + " Data: "+ data+ "  "+ size();
+	}
 		
-		@Override
-		public int size() {
-			return data.length();
-		}
-
-	
+	@Override
+	public int size() {
+		return data.length();
+	}
 
 }
